92fec2e46fa5b8935caa39365768485aeb9ec11e
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-19 19:03:29 +0000

    Layout Component

--- authappcourse/pages/login.js
+++ authappcourse/pages/login.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import Layout from "../layout/layout";
 export default function Login() {
   return (
-    <div>
+    <Layout>
       <Head>
         <title> Login </title>
       </Head>
       <h1> Login</h1>
-    </div>
+    </Layout>
   );
 }
