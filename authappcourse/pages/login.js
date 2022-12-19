ba48709a06403fc2c80966b37ad4b960f782e164
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-19 18:25:06 +0000

    Project Setup

---
+++ authappcourse/pages/login.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Login() {
+  return (
+    <div>
+      <Head>
+        <title> Login </title>
+      </Head>
+      <h1> Login</h1>
+    </div>
+  );
+}
