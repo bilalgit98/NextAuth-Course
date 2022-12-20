eb7f7240e8a6bedccdf9b72c4788482f68721afd
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-20 23:24:55 +0000

    Homepage

--- authappcourse/pages/index.js
+++ authappcourse/pages/index.js
@@ -1,17 +1,57 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
+  const [session, setSession] = useState(false);
   return (
     <div className={styles.container}>
       <Head>
         <title>HOMEPAGE</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </main>
+      {session ? authorisedUser() : Guest()}
     </div>
+  );
+}
+
+//guest component
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">Guest Homepage</h3>
+
+      <div className="flex justify-center">
+        <Link legacyBehavior href={"/login"}>
+          <a className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50">
+            Sign In
+          </a>
+        </Link>
+      </div>
+    </main>
+  );
+}
+//authorised user component
+function authorisedUser() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">Authorised User Homepage</h3>
+
+      <div className="details">
+        <h5> UNKNOWN USERANME</h5>
+        <h5> UNKNOWN</h5>
+      </div>
+      <div className="flex justify-center">
+        <button className="mt-5 px-10 py-1 rounded-sm  bg-indigo-500 text-gray-50">
+          Sign Out
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Link legacyBehavior href={"/profile"}>
+          <a className="mt-5 px-10 py-1 rounded-sm bg-gray-50">Profile</a>
+        </Link>
+      </div>
+    </main>
   );
 }
