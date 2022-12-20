4b2c09f3f72f39519e7721c5660b7dc2f6f35577
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-20 17:23:17 +0000

    Login Panel

--- authappcourse/layout/layout.js
+++ authappcourse/layout/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
             <div className={styles.cloud_two}></div>
           </div>
 
-          <div className="right flex flex-col justify-evenly bg-slate-500">
+          <div className="right flex flex-col justify-evenly">
             <div className="text-center py-10"> {children}</div>
           </div>
         </div>
--- authappcourse/pages/login.js
+++ authappcourse/pages/login.js
@@ -1,12 +1,43 @@
 import Head from "next/head";
 import Layout from "../layout/layout";
+import Link from "next/link";
 export default function Login() {
   return (
     <Layout>
       <Head>
         <title> Login </title>
       </Head>
-      <h1> Login</h1>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
+          <p className="w-3/4 mx-auto text-gray-400 "> lorem text </p>
+        </div>
+
+        <form className="flex flex-col gap-5">
+          <div className="input-group">
+            <input type="email" name="email" placeholder="Email!" />
+          </div>
+
+          <div className="input-group">
+            <input type="password" name="password" placeholder="Password!" />
+          </div>
+
+          <div className="input-button">
+            <button type="submit">Login</button>
+          </div>
+          <div className="input-button">
+            <button type="submit">Sign In with Google</button>
+          </div>
+          <div className="input-button">
+            <button type="submit">Sign In with GitHub</button>
+          </div>
+        </form>
+
+        <p className="text-center">
+          Dont Have a account yet? click here
+          <Link href={"/register"} legacyBehavior><a className="text-blue-700"> Sign Up</a></Link>
+        </p>
+      </section>
     </Layout>
   );
 }
