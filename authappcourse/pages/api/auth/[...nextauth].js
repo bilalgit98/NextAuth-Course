f7ca76bc7b1c06549553648f5d8a7cb657be2005
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-21 18:37:46 +0000

    NextAuth

---
+++ authappcourse/pages/api/auth/[...nextauth].js
@@ -0,0 +1,6 @@
+import NextAuth from "next-auth";
+const client_id =
+  "783008345734-5135ff83bef000cfifqple2brl6blmo0.apps.googleusercontent.com";
+
+const client_secret = "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";
+export default NextAuth[{}];
